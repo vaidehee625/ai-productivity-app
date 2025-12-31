a18d7e2f98be13affdbd90eb575c7b6c6e24c04d
Vaidehee Kumari <vaideheekumari2802@gmail.com>
2025-12-31 12:21:00 +0530

    Trigger Netlify redeploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Add token to requests
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    config.headers.authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
